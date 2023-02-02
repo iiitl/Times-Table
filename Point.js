99d61a866cf568e98c6b986997ea7a4208dd6a74
SameepAher <sameepaher@gmail.com>
2023-02-02 16:57:11 +0530

    Fixed indentation

--- Point.js
+++ Point.js
@@ -1,11 +1,11 @@
 class Point {
-    constructor (x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    draw () {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, 2, 0, Math.PI * 2, true);
-        ctx.fill();
-    }
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 2, 0, Math.PI * 2, true);
+    ctx.fill();
+  }
 }
